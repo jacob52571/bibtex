8ec1e0361b1908d4d451f88128736404d0c73a46
Jacob <lijacob290@gmail.com>
2025-02-07 11:52:49 -0700

    made arxiv links get bibtex from arxiv

--- bibtex.py
+++ bibtex.py
@@ -3,13 +3,22 @@ import zipfile
 import xml.etree.ElementTree as ET
 import sys
 import urllib.request
+import shutil
 
 def handle_arxiv_links(link):
-    # if the links are from arxiv, i want the pdf link only
-    # link format: https://arxiv.org/abs/1604.07316
     link_data = link.split("/")
     paper_id = link_data[-2] if link_data[-1] == "" else link_data[-1]
-    return "https://arxiv.org/pdf/" + paper_id
+    # if the links are from arxiv, check if they have a bibtex citation available already
+    try:
+        file_contents = urllib.request.urlopen("https://arxiv.org/bibtex/" + paper_id).read().decode("utf-8")
+        return [True, file_contents]
+    except:
+        
+        return [False, "https://arxiv.org/pdf/" + paper_id]
+
+
+    # if the links are from arxiv, i want the pdf link only
+    
 
 if __name__ == "__main__":
     current_path = os.path.abspath(os.path.dirname(__file__))
@@ -18,6 +27,7 @@ if __name__ == "__main__":
     link_file = current_path + "/" + input("Enter the name/path of the word document: ")
 
     links = []
+    citations = []
     try:
         # format the file 
         os.rename(link_file, link_file + ".zip")
@@ -31,8 +41,17 @@ if __name__ == "__main__":
             relationships = tree.getroot()
             for relationship in relationships:
                 if "TargetMode" in relationship.attrib and relationship.attrib["TargetMode"] == "External" and relationship.attrib["Target"][0:4] == "http":
-                    links.append(relationship.attrib["Target"])
-
+                    link = relationship.attrib["Target"]
+                    # if the link is arxiv, get the bibtex automatically if possible, otherwise add the pdf link
+                    if "arxiv" in link:
+                        link = handle_arxiv_links(link)
+                        if link[0]:
+                            citations.append(link[1])
+                        else:
+                            links.append(link[1])
+                    else:
+                        links.append(link)
+                    
         # print the result
         print(len(links), "links found!")
 
@@ -44,10 +63,6 @@ if __name__ == "__main__":
         # get the files from the links
         for i in range(0, len(links)):
             link = links[i]
-
-            # if the link is arxiv, ensure it is the pdf link
-            if "arxiv" in link:
-                link = handle_arxiv_links(link)
             
             # try to get the file from urllib and using curl if that fails
             file_contents = ""
@@ -61,6 +76,14 @@ if __name__ == "__main__":
         
         print("All pdfs downloaded, starting processing now...")
 
+        for file in os.walk(pdf_paths):
+            # check that the file is actually a pdf
+            with open(file, "r") as f:
+                pass
+
+        with open("citations.txt", "w") as f:
+            for line in citations:
+                f.write(line + "\n")
     except FileNotFoundError:
         # error handling if file name was wrong
         print("File was not found.")
@@ -70,5 +93,7 @@ if __name__ == "__main__":
         try:
             # make the zip back into word
             os.rename(link_file + ".zip", link_file)
+            # remove the folder
+            shutil.rmtree(current_path + "/word_file/")
         finally:
             pass
